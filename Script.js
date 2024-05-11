9918558a6af7e492ae03ab4ed14b6919e564f781
Rajnish0311 <153858423+Rajnish0311@users.noreply.github.com>
2024-05-11 13:31:31 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,8 @@
+console.log("chal rahi hai");
+document.querySelector('.button').
+addEventListener('click',function name(e) {
+    e.preventDefault()
+
+document.querySelector('.result').innerHTML= `Failed`
+    
+})
